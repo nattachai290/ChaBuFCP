0f10c4057d5e38d58ff662813e15861d545d730e
arm <natttachai6317@gmail.com>
2016-03-24 04:17:50 +0700

    test fragment by arm

--- app/src/main/java/com/app/fcp/chabufcp/NavigationUser.java
+++ app/src/main/java/com/app/fcp/chabufcp/NavigationUser.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
@@ -15,6 +17,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
+
+import com.app.fcp.chabufcp.fragment.FirstFragment;
 
 public class NavigationUser extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private final String MSG_NavigateUser = "NavigateUser";
@@ -28,6 +32,17 @@ public class NavigationUser extends AppCompatActivity implements NavigationView.
         setContentView(R.layout.activity_navigation_user);
         Toolbar toolbar = (Toolbar) findViewById(R.id.nav_toolbar);
         setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+                order();
+
+            }
+        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -118,15 +133,29 @@ public class NavigationUser extends AppCompatActivity implements NavigationView.
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
-
+        Fragment fragment = null;
+        Class fragmentClass = null;
         if (id == R.id.nav_profile) {
             // Handle the camera action
+            fragmentClass = FirstFragment.class;
+
+            try {
+                fragment = (Fragment) fragmentClass.newInstance();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            fragmentManager.beginTransaction().replace(R.id.flContent, fragment).commit();
+            item.setChecked(true);
+
         } else if (id == R.id.nav_logout) {
             Intent main = new Intent(this,MainActivity.class);
             startActivity(main);
-
-
         }
+
+
+
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
@@ -136,6 +165,8 @@ public class NavigationUser extends AppCompatActivity implements NavigationView.
 
     }
 
+    public void order(){
 
+    }
 
 }
